c66d134589cc1ebbc33d4606c2f56ced175e43cc
dgahle <dsgahle1771@outlook.com>
2022-12-03 21:50:40 +0000

    C++ initial set up. main.cpp and .h file as well as the CMakeLists.

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by dsgah on 03/12/2022.
+//
+
+#include "main.h"
+
+// main
+int main() {
+
+}
---
+++ cpp/main.h
@@ -0,0 +1,8 @@
+//
+// Created by dsgah on 03/12/2022.
+//
+
+#ifndef PDF_GRID_MAIN_H
+#define PDF_GRID_MAIN_H
+
+#endif //PDF_GRID_MAIN_H
