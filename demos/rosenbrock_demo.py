0add9fbf35dba7d09f36311e676153cdb48acc48
dgahle <dsgahle1771@outlook.com>
2022-12-03 21:32:56 +0000

    rosenbrock_demo.py: Minor formatting and commenting.

--- demos/rosenbrock_demo.py
+++ demos/rosenbrock_demo.py
@@ -1,10 +1,10 @@
 
-from numpy import array, where, zeros, exp
 from imageio.v2 import mimwrite, imread
-from os import remove
-from itertools import chain
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
+from numpy import array, where, zeros, exp
+from os import remove
+from itertools import chain
 
 from pdfgrid import PdfGrid
 
@@ -25,49 +25,54 @@ def rosenbrock(t):
     v = 3   # variance of the gaussian term
     return -X2 - b*(Y - X2)**2 - 0.5*(X2 + Y**2)/v
 
-grid_spacing = array([0.1,0.1])
-SPG = PdfGrid(2)
+
+def main() -> None:
+    # Set up adaptive grid sampler
+    grid_spacing = array([0.1,0.1])
+    SPG = PdfGrid(2)
+    # Run adaptive grid sampling
+    image_id = 0
+    files = []
+    while SPG.state != "end":
+        P = [rosenbrock(theta*grid_spacing) for theta in SPG.to_evaluate]
+        SPG.update_cells(P)
+        SPG.take_step()
+        # Create image showing the progress in mapping the posterior
+        grid = zeros([60, 60]) + 2
+        for v,p in zip(SPG.indices, SPG.probability):
+            i,j = v
+            grid[i+30,j+30] = p
+
+        inds = where(grid == 2)
+        grid = exp(grid)
+        grid[inds] = -1
+
+        current_cmap = get_cmap()
+        current_cmap.set_under('white')
+
+        filename = 'rosenbrock_{}.png'.format(image_id)
+        files.append(filename)
+
+        fig = plt.figure(figsize = (8,8))
+        ax = fig.add_subplot(111)
+        ax.imshow(grid.T, interpolation='nearest', vmin=0., vmax=1.)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.tight_layout()
+        plt.savefig(filename, dpi = 50)
+        plt.close()
+
+        image_id += 1
+    # Build GIF to show the evolution of the adaptive grid sampler
+    images = []
+    for filename in chain(files, [files[-1]]*20):
+        images.append(imread(filename))
+    # Save the GIF
+    mimwrite('PdfGrid.gif', images, duration = 0.05)
+    # Delete the composit images
+    for filename in files:
+        remove(filename)
 
 
-image_id = 0
-files = []
-while SPG.state != "end":
-    P = [rosenbrock(theta*grid_spacing) for theta in SPG.to_evaluate]
-    SPG.update_cells(P)
-    SPG.take_step()
-
-
-    grid = zeros([60, 60]) + 2
-    for v,p in zip(SPG.indices, SPG.probability):
-        i,j = v
-        grid[i+30,j+30] = p
-
-    inds = where(grid == 2)
-    grid = exp(grid)
-    grid[inds] = -1
-
-    current_cmap = get_cmap()
-    current_cmap.set_under('white')
-
-    filename = 'rosenbrock_{}.png'.format(image_id)
-    files.append(filename)
-
-    fig = plt.figure(figsize = (8,8))
-    ax = fig.add_subplot(111)
-    ax.imshow(grid.T, interpolation='nearest', vmin=0., vmax=1.)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.tight_layout()
-    plt.savefig(filename, dpi = 50)
-    plt.close()
-
-    image_id += 1
-
-images = []
-for filename in chain(files, [files[-1]]*20):
-    images.append(imread(filename))
-
-mimwrite('PdfGrid.gif', images, duration = 0.05)
-
-for filename in files:
-    remove(filename)
+if __name__ == "__main__":
+    main()
