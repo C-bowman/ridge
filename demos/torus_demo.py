dc9ab097aa496c4ea0f3fc3d578abb11660df04f
C-bowman <chris.bowman.physics@gmail.com>
2024-04-30 18:06:20 +0100

    applied Black formatting to the code

--- demos/torus_demo.py
+++ demos/torus_demo.py
@@ -5,13 +5,13 @@ from ridge import Ridge
 
 class ToroidalGaussian:
     def __init__(self):
-        self.R0 = 1.
+        self.R0 = 1.0
         self.eps = 0.10
-        self.coeff = -0.5 / (self.R0*self.eps)**2
+        self.coeff = -0.5 / (self.R0 * self.eps) ** 2
 
     def __call__(self, theta):
         x, y, z = theta
-        r_sqr = z**2 + (sqrt(x**2 + y**2) - self.R0)**2
+        r_sqr = z**2 + (sqrt(x**2 + y**2) - self.R0) ** 2
         return self.coeff * r_sqr
 
 
@@ -20,7 +20,7 @@ posterior = ToroidalGaussian()
 
 # specify settings for the grid
 grid_spacing = array([0.04, 0.04, 0.02])
-grid_centre = array([0., 0., 0.])
+grid_centre = array([0.0, 0.0, 0.0])
 grid_bounds = array([[-1.5, -1.5, -0.5], [1.5, 1.5, 0.5]]).T
 
 # create a PdfGrid instance
